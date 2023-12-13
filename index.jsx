e47fffa489f9dc31bd739922ea71e0c8057e7751
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2023-12-13 17:00:53 +0530

    change in index.js

--- index.jsx
+++ index.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from "./components/Menu/index"
 import Star from "./components/Star"
+import Badge from "./components/Badge"
 
 function App() {
   return (
-    <h1>Your components go here</h1>
+    <>
+      <h1>Your components go here</h1>
+      <Badge
+        color="grey"
+        shape="pill"
+      >
+        Hello
+      </Badge>
+    </>
   )
 }
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
